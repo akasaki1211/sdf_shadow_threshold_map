792def577a34eeea4c911237610944b55799e5e8
akasaki1211 <akasaki1211ab@gmail.com>
2024-06-27 22:29:10 +0900

    refactor: refactor run.py

--- run.py
+++ run.py
@@ -4,25 +4,32 @@ import argparse
 import sdftool
 
 def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--inputdir", type=str, default='input', help='Input directory path')
-    parser.add_argument("-o", "--outputdir", type=str, default='output', help='Output directory path')
-    parser.add_argument("-n", "--outputname", type=str, default='shadow_threshold_map', help='Output file name')
-    parser.add_argument("-b", "--bitdepth", type=int, choices=[8, 16], default=8, help='Output PNG bit depth (8 or 16)')
-    parser.add_argument("-r", "--reverse", type=bool, default=False, help='Reverse the gradient direction')
-    parser.add_argument("-t", "--savetemp", type=bool, default=False, help='Save the image during processing')
+    parser = argparse.ArgumentParser(description="Create a Shadow Threshold Map by interpolating SDF (Signed Distance Field).")
+    parser.add_argument("-i", "--inputdir", type=str, required=True, help="Specify the input directory path where images are stored. This is a required option.")
+    parser.add_argument("-o", "--outputdir", type=str, default='output', help="Specify the output directory path where images will be saved. Default is 'output'.")
+    parser.add_argument("-n", "--outputname", type=str, default='shadow_threshold_map', help="Specify the base name for the output PNG file. Default is 'shadow_threshold_map'.")
+    parser.add_argument("-b", "--bitdepth", type=int, choices=[8, 16], default=8, help="Set the bit depth for output PNG files. Valid options are 8 or 16. Default is 8.")
+    parser.add_argument("-r", "--reverse", action='store_true', help="Enable to reverse the gradient direction.")
+    parser.add_argument("-t", "--savetemp", action='store_true', help="Enable to save intermediate images during the processing.")
     return parser.parse_args()
 
-if __name__ == "__main__":
+def get_file_list(directory):
+    filelist = []
+    for f in os.listdir(directory):
+        path = os.path.join(directory, f)
+        if os.path.isfile(path):
+            filelist.append(path)
+    return filelist
 
+def main():
     opt = parse_arguments()
 
     # load input files
-    filelist = []
-    for f in os.listdir(opt.inputdir):
-        path = os.path.join(opt.inputdir, f)
-        if os.path.isfile(path):
-            filelist.append(path)
+    try:
+        filelist = get_file_list(opt.inputdir)
+    except FileNotFoundError:
+        print("Error: {} does not exist.".format(opt.inputdir))
+        return
     
     # set output path
     output_path = os.path.join(os.path.abspath(opt.outputdir), opt.outputname + '.png')
@@ -35,3 +42,6 @@ if __name__ == "__main__":
         reverse=opt.reverse, 
         save_temp=opt.savetemp
     )
+
+if __name__ == "__main__":
+    main()
